a736a70141e3575a15a39a523b13d13ab3557f2e
meedaah19 <hameedattimileyinoyewopo@gmail.com>
2026-04-27 21:35:14 +0100

    feat: uncomment expenseForm function and related code for signup API integration

--- react-ts/src/api/expense.ts
+++ react-ts/src/api/expense.ts
@@ -1,33 +1,29 @@
-// const API_BASE_URL = 'http://localhost:5000/expense';
+const API_BASE_URL = 'http://localhost:5000/expense';
 
-// type expenseProp = {
+type expenseProp = {
 
-// }
+}
 
-// export async function expenseForm({  }: expenseProp) {
-//     const response = await fetch(`${API_BASE_URL}/signup`, {
-//         method: 'POST',
-//         headers: {
-//             'Content-Type': 'application/json'
-//         },
-//         body: JSON.stringify({
-//             name,
-//             email,
-//             password
-//         })
-//     })
-//     const data = await response.json()as {
-//         message?: string;
-//         user?: any;
-//         token?: string;
-//         error?: string; 
-//     }
-//     if (!response.ok) {
-//     throw new Error(data.error || data.message || "Signup failed");
-//     }
-//     localStorage.setItem("token", data.token || "");
-//     localStorage.setItem("tokenExpiry",(Date.now() + 60 * 60 * 1000).toString());
-//     return data;
+export async function expenseForm({  }: expenseProp) {
+    const response = await fetch(`${API_BASE_URL}/signup`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            
+        })
+    })
+    const data = await response.json()as {
+        message?: string;
+        user?: any;
+        token?: string;
+        error?: string; 
+    }
+    if (!response.ok) {
+    throw new Error(data.error || data.message || "Signup failed");
+    }
+    return data;
     
     
-// }
+}
