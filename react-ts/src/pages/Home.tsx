8d411d460ddc4558bf242034d502ea13c26e499e
meedaah19 <hameedattimileyinoyewopo@gmail.com>
2026-05-16 21:25:42 +0100

    feat: update styling in Home component; adjust padding and font sizes for improved layout and accessibility

--- react-ts/src/pages/Home.tsx
+++ react-ts/src/pages/Home.tsx
@@ -17,12 +17,12 @@ export default function Home() {
     <PageLoading className="relative">
     <div className="absolute inset-0 min-h-screen bg-[url('/background.jpg')] bg-cover bg-center">
         <div className="absolute inset-0 bg-black/70 "/>
-         <div className="relative z-10 md:py-5 py-25 min-h-screen">
+         <div className="relative z-10 md:py-5 py-10 min-h-screen">
         <div className="w-full px-4 sm:px-6 md:px-0 md:max-w-3xl md:mx-auto text-center">
-            <h1 className="font-bold text-4xl text-text-primary"> Welcome to FinSight</h1>
+            <h1 className="font-bold md:text-4xl text-3xl text-text-primary"> Welcome to FinSight</h1>
             <p className="mt-4 font-medium text-text-primary">Track smarter. <strong>Spend wiser.</strong> </p>
 
-            <div className="flex flex-col md:mt-7 mt-10 md:space-y-3 space-y-5">
+            <div className="flex flex-col md:mt-7 mt-8 md:space-y-3 space-y-5">
                 <motion.div 
                 whileHover={{
                     y: -10,
@@ -33,8 +33,8 @@ export default function Home() {
                     stiffness: 300,
                     damping: 15,
                 }}
-                className="border w-full max-w-md font-medium mx-auto flex md:gap-5 gap-3 rounded-2xl shadow-2xl text-text-primary p-3">
-                <Wallet className="text-blue-400" size={20} />
+                className="border w-full max-w-md font-medium mx-auto flex md:gap-5 gap-10 rounded-2xl shadow-2xl text-text-primary p-3 md:py-3 py-4.5">
+                <Wallet className="text-blue-400" size={28} />
                    Financial clarity starts here.
                 </motion.div>
                 <motion.div 
@@ -47,8 +47,8 @@ export default function Home() {
                     stiffness: 300,
                     damping: 15,
                 }}
-                className="border w-full max-w-md flex font-medium mx-auto md:gap-5 gap-3 rounded-2xl shadow-2xl text-text-primary p-3">
-                <BarChart3 className="text-green-400" size={20} />
+                className="border w-full max-w-md flex font-medium mx-auto md:gap-5 gap-3 rounded-2xl shadow-2xl text-text-primary p-3 py-2 md:py-3">
+                <BarChart3 className="text-green-400" size={28} />
                     Your personal finance insight dashboard.
                 </motion.div>
                 <motion.div 
@@ -61,8 +61,8 @@ export default function Home() {
                     stiffness: 300,
                     damping: 15,
                 }}
-                className="border w-full max-w-md flex font-medium mx-auto md:gap-5 gap-3 rounded-2xl shadow-2xl text-text-primary py-3 pl-3">
-                <TrendingUp className="text-yellow-400" size={20} />
+                className="border w-full max-w-md flex font-medium mx-auto md:gap-5 gap-3 rounded-2xl shadow-2xl text-text-primary pl-3 py-2 md:py-3">
+                <TrendingUp className="text-yellow-400" size={28} />
                    Turn your spending into smarter decisions.
                 </motion.div>
                 <motion.div 
@@ -75,8 +75,8 @@ export default function Home() {
                         stiffness: 300,
                         damping: 15,
                     }}
-                    className="border w-full max-w-md flex font-medium mx-auto md:gap-5 gap-3 rounded-2xl shadow-2xl text-text-primary p-3">
-                    <PiggyBank className="text-pink-400" size={20} />
+                    className="border w-full max-w-md flex font-medium mx-auto md:gap-5 gap-3 rounded-2xl shadow-2xl text-text-primary p-3 py-2 md:py-3">
+                    <PiggyBank className="text-pink-400" size={28} />
                         Your personal finance insight dashboard.
                     </motion.div>
                     <motion.div 
@@ -89,13 +89,13 @@ export default function Home() {
                         stiffness: 300,
                         damping: 15,
                     }}
-                    className="border w-full max-w-md flex font-medium mx-auto md:gap-5 gap-3 rounded-2xl shadow-2xl text-text-primary p-3">
-                    <Eye className="text-purple-400" size={20} />
+                    className="border w-full max-w-md flex font-medium mx-auto md:gap-5 gap-11 rounded-2xl shadow-2xl text-text-primary p-3 md:py-3 py-4.5">
+                    <Eye className="text-purple-400" size={28} />
                     See your money differently.
                     </motion.div>
                 </div>
 
-            <div className="flex flex-col md:mt-5 mt-10 space-y-5">
+            <div className="flex flex-col md:mt-5 mt-5 md:sapce-y-5 space-y-4">
                 <button 
                 onClick={handleCreateAccount} 
                 className="border bg-blue-500 hover:bg-blue-600 w-full max-w-md hover:scale-105 transition mx-auto gap-5 rounded-2xl shadow-2xl text-text-primary font-medium px-5 py-3 flex justify-center">Create Account</button>
